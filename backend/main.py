fc41eb741f2258558064ab23b1c017d10dbbbb21
DerenB <dbozerprogramming@gmail.com>
2023-09-25 11:41:07 -0400

    CORS

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ origins = [
     'http://localhost:3000',
     'https://localhost:3000',
     'http://127.0.0.1:8000/',
+    'http://127.0.0.1:8080/',
     'https://seal-app-8qhxt.ondigitalocean.app'
 ]
 
